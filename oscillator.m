397b57040362f88c2183423bac0d494612590ceb
Mario Juarez <mario.glaro@gmail.com>
2016-05-06 12:35:12 +0100

    First draft of the oscillator model
    x is unknown and has to be determined

---
+++ oscillator.m
@@ -0,0 +1,34 @@
+
+function dy = oscillator(t,y) 
+%solver: [t,y] = ode45(@oscillator,[1000],ones(4,1))
+%plot(t,y)
+% Model parameters 
+Ca = 1;
+Ci = 4;
+d = 1e-3;
+a = 2500;
+tu = 10;
+k = 1;
+k_1 = 1e-1;
+b = 6e-2;
+ya = 15;
+yi = 24;
+yh = 1e-2;
+f = 3e-1;
+g = 1e-2;
+d0 = 88e-2;
+D = 25e-1;
+D_1 = 0; % micrometer^2/sec, they use 0, 800 and 4000 in the paper 
+u = 1; %in the paper they use values from 1-2
+t = 1200; % the time used in the paper covers a wide range from 0 to >1200
+x = 
+
+Ht = dy(3)*(t-tu);
+P = (d+(a*Ht^2))/(1+(k_1*Ht^2));
+
+dy(1) = Ca*(1-(d/d0)^4)*P-(ya*y(1)/(1+f*(y(1)+y(2))));% Eq. for AiiA
+dy(2) = Ci*(1-(d/d0)^4)*P-(yi*y(2)/(1+f*(y(1)+y(2))));% Eq. for LuxI
+dy(3) = (b*y(2))/2+(k*y(2))-((yh*y(1)*y(3))/1+(g*y(1)))+D*(dy(4)- dy(3));
+dy(4) = -((d/(1-d))*D*(y(4)-y(3)))-u*y(4)+D_1*((dy(4))^2/dy(x^2));
+
+end 
