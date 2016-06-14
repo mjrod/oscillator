866514e62be1bdb7d4312aec74f87fb6518c313b
Mario <mario.glaro@gmail.com>
2016-06-14 16:46:02 +0100

    Add files via upload
    First attempt with the oscillator. It takes too long, but it apparently works. Problems with the lags solved.

--- oscillator.m
+++ oscillator.m
@@ -1,12 +1,13 @@
-
-function dy = oscillator(t,y) 
-%plot(t,y)
+function dy = oscillator(t,y,Z) 
+%solver: sol = dde23(@oscillator,[10, 1],ones(3,1),[0,540]);
+%plot(sol.x,sol.y)
+t
 % Model parameters 
 Ca = 1;
 Ci = 4;
 d = 1e-3;
 a = 2500;
-tu = 10;
+% tu = 10;
 k = 1;
 k_1 = 1e-1;
 b = 6e-2;
@@ -19,10 +20,13 @@ d0 = 88e-2;
 D = 25e-1;
 D_1 = 0; % micrometer^2/sec, they use 0, 800 and 4000 in the paper 
 u = 1; %in the paper they use values from 1-2
-t = 1200; % the time used in the paper covers a wide range from 0 to >1200
+% tp = 1200; % the time used in the paper covers a wide range from 0 to >1200
+ylag3 = Z(:,1);
+ylag2 = Z(:,2);
+dy = zeros(3,1);
 
-Ht = y(3)*(t-tu);
-P = (d+(a*Ht^2))/(1+(k_1*Ht^2));
+Ht = ylag3(1);
+P = (d+(a*(Ht).^2))/(1+(k_1*(Ht).^2));
 
 dy(1) = Ca*(1-(d/d0)^4)*P-(ya*y(1)/(1+f*(y(1)+y(2))));% Eq. for AiiA
 dy(2) = Ci*(1-(d/d0)^4)*P-(yi*y(2)/(1+f*(y(1)+y(2))));% Eq. for LuxI
