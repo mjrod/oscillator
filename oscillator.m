ee58ea24506b50d6c7b5c4d72b6a5efd10561819
PhRausch <philipp.rauschendorfer@googlemail.com>
2016-06-23 11:25:01 +0100

    Added the 4th equation
    I added the 4th eq regarding the external AHL concentration to simulate
    the desired oscillation. However I excluded the diffusion part(Fick's
    second law).
    Once period of oscillation can now be seen in the plot.

--- oscillator.m
+++ oscillator.m
@@ -19,18 +19,20 @@ g = 1e-2;
 d0 = 88e-2;
 D = 25e-1;
 % D_1 = 0; % micrometer^2/sec, they use 0, 800 and 4000 in the paper 
-% u = 1; %in the paper they use values from 1-2
+u = 1; %in the paper they use values from 1-2
 % tp = 1200; % the time used in the paper covers a wide range from 0 to >1200
-ylag1 = Z(:,1);
+ylag1 = Z;
 % ylag2 = Z(:,2);
-dy = zeros(3,1);
+dy = zeros(4,1);
 
 
 % Ht = ylag3(1);
-P = (d+(a*((ylag1(3)).^2)))/(1+(k_1*((ylag1(3)).^2)));
+ 
+P = (d+(a*((y(3)*ylag1(3)).^2)))/(1+(k_1*((y(3)*ylag1(3)).^2)));
 
-dy(1) = Ca*(1-(d/d0)^4)*P-(ya*y(1)/(1+f*(y(1)+y(2))));% Eq. for AiiA
-dy(2) = Ci*(1-(d/d0)^4)*P-(yi*y(2)/(1+f*(y(1)+y(2))));% Eq. for LuxI
-dy(3) = (b*y(2)/(1+(k*y(2))))-(yh*y(1)*y(3)/(1+(g*y(1))))+D*1;
+dy(1) = Ca*(1-((d/d0)^4))*P-(ya*y(1)/(1+f*(y(1)+y(2))));% Eq. for AiiA
+dy(2) = Ci*(1-((d/d0)^4))*P-(yi*y(2)/(1+f*(y(1)+y(2))));% Eq. for LuxI
+dy(3) = (b*y(2)/(1+(k*y(2))))-(yh*y(1)*y(3)/(1+(g*y(1))))+D*(y(4)-y(3));
+dy(4) = (-d/(1-d))*D*(y(4)-y(3))-u*y(4);
 
 end 
